489caed5340163c04dc7b690cf25d24ddbb7af1f
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-05 19:50:37 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./course/course.component";
-import {CourseResolver} from "./services/course.resolver";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { CourseResolver } from './services/course.resolver';
 
 const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'courses/:id',
+    component: CourseComponent,
+    resolve: {
+      course: CourseResolver,
     },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'courses/:id',
-        component: CourseComponent,
-        resolve: {
-            course: CourseResolver
-        }
-    },
-    {
-        path: "**",
-        redirectTo: '/'
-    }
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,20 +1,16 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css'],
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
 
-    @Input()
-    courses: Course[];
+  constructor() {}
 
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
-
+  ngOnInit() {}
 }
