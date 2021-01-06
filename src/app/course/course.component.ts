830e7d9689f50bb6917ed4f0dc1174500d852eeb
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-06 02:52:22 +0200

    add

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,6 +6,7 @@ import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { debounceTime, distinctUntilChanged, startWith, tap, timeout } from 'rxjs/operators';
 import { merge, fromEvent } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'course',
@@ -14,15 +15,17 @@ import { merge, fromEvent } from 'rxjs';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
   course: Course;
+  dataSource = new MatTableDataSource([]);
+  displayedColumns = ["seqNo", "description", "duration"];
 
   constructor(private route: ActivatedRoute, private coursesService: CoursesService) {}
 
   ngOnInit() {
-    console.log(this.route);
-    
     this.course = this.route.snapshot.data['course'];
-    console.log(this.course);
-    
+    this.coursesService.findAllCourseLessons(this.course.id)
+      .subscribe(
+        lessons => this.dataSource.data = lessons
+      );
   }
 
   ngAfterViewInit() {}
