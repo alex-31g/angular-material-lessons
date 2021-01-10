a9116fdd02e3e34e862388cbecef0715031b9036
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-10 02:35:24 +0200

    add

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -46,25 +46,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
     //   .subscribe(lessons => this.dataSource.data = lessons);
 
     this.dataSource = new LessonsDatasource(this.coursesService);
+
+    // Загрузка данных для таблицы при первом переходе на страницу
+    this.dataSource.loadLessons(this.course.id, '', 'asc', 0, 3);
   }
 
   ngAfterViewInit() {
     // this.paginator.page - это Observable, который эмитит события,
     // когда юзер будет взаимодействовать с MatPaginator внутри html
     this.paginator.page.pipe(
-      // startWith - не дожидаясь получения первого значения от Observable,
-      // начинает поток с заданного в startWith значения, то-есть в subscribe()
-      // первым попадет то значение, которое мы задали в startWith.
-      // Зачем мы используем здесь startWith?
-      // MatPaginator начинает эмитить данные только при взаимодействии
-      // с юзером, а при первом переходе на страницу с уроками уже
-      // необходимо отобразить начальные данные, но они не могут быть получены,
-      // так как юзер еще не взаимодействовал с MatPaginator, 
-      // а значит и запроса к серверу еще не было.
-      // Чтобы выполнить запрос - мы с помощью MatPaginator запускаем поток,
-      // вследствии чего сработает следующий за ним оператор tap,
-      // который и запустить обращение к серверу
-      startWith(null as string),
+      // MatPaginator начинает эмитить данные при взаимодействии с юзером -
+      // производим загрузку данных в зависимости от данных,
+      // полученных с потока
       tap(() => {
         this.dataSource.loadLessons(this.course.id, '', 'asc', this.paginator.pageIndex, this.paginator.pageSize);
       })
