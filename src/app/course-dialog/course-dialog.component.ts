207195c675052c138e58e581ecaff73cb6e8255a
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-11 20:02:43 +0200

    add

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -10,11 +10,45 @@ import * as moment from 'moment';
   styleUrls: ['./course-dialog.component.css'],
 })
 export class CourseDialogComponent implements OnInit {
-  constructor() {}
+
+  description: string;
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+
+    // Инжектим MatDialogRef, который принимает параметр, который
+    // соответствует компоненту, в котором находится тело dialog-окна
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+
+    // Чтобы получить данные dialogConfig.data, которые были отправлены 
+    // из courses-card-list компонента методом editCourse,
+    // нам необходимо заинжектить MAT_DIALOG_DATA
+    @Inject(MAT_DIALOG_DATA) { description, longDescription, category }: Course // <-- 1 вариант
+    // @Inject(MAT_DIALOG_DATA) data: Course // <-- 2 вариант (более понятная запись строки выше)
+
+  ) {
+    this.description = description; // <-- 1
+    // this.description = data.description; // <-- 2
+
+    this.form = fb.group({
+      description: [description, Validators.required],
+      category: [category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [longDescription, Validators.required],
+    });
+  }
 
   ngOnInit() {}
 
-  save() {}
+  save() {
+    // закрываем dialog-окно и передаем данные формы,
+    // которые будут получены в course-card-list компоненте
+    this.dialogRef.close(this.form.value);
+  }
 
-  close() {}
+  close() {
+    // закрываем dialog-окно
+    this.dialogRef.close();
+  }
 }
--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -12,9 +12,31 @@ export class CoursesCardListComponent implements OnInit {
   @Input()
   courses: Course[];
 
-  constructor() {}
+  // Инжектим MatDialog
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {}
 
-  editCourse({ description, longDescription, category }: Course) {}
+  // При клике EDIT - срабатывает метод editCourse
+  editCourse({ description, longDescription, category }: Course) {
+    // Объект конфигурации для dialog-окна
+    const dialogConfig = new MatDialogConfig();
+
+    // autoFocus - поместить фокус на первый элемент формы
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = { description, longDescription, category };
+    
+    // open - открывает dialog-окно.
+    // 1й параметр - имя компонента, который должен быть открыт;
+    // 2й параметр - объект конфигурации, который будет получен внутри CourseDialogComponent.
+    // open() возвращает экземпляр MatDialogRef, с помощью которого можно закрыть окно 
+    // с помощью метода dialogRef.close (см. course-dialog компонент),
+    // а также с помощью метода dialogRef.afterClosed - получить данные формы
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    // Получаем данные формы, после её закрытия в course-dialog компоненте
+    dialogRef.afterClosed().subscribe(
+      val => console.log('Dialog output', val)
+    )
+  }
 }
